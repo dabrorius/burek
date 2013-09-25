4d9ee9b63df4c93b423f4910e8e7867009e217e4
Filip Defar <dabrorius@gmail.com>
2013-09-26 01:18:19 +0200

    Added handling of translation key generation

--- tasks/tasks.rb
+++ tasks/tasks.rb
@@ -9,6 +9,7 @@ namespace :burek do
     search_folders = ['./app/views/**/*']
     translation_path = './config/locales/burek/'
     translation_placeholder = 'TODO'
+    ignore_folders_for_key = ['.','app']
     locales = ['en','fi']
     # =========
 
@@ -23,10 +24,17 @@ namespace :burek do
       Dir.glob(folder) do |file_name|
         unless File.directory?(file_name)
           file = File.open(file_name, "rb")
+          path = file_name.split('/')
+          path.delete_if do |item|
+            ignore_folders_for_key.include? item
+          end
+          path.pop
+          filtered_path = path.join('/')
           contents = file.read
           matches = find_burek_calls(contents)
           matches.each do |value|
-            key = value.downcase.gsub(' ','_')
+            key = filtered_path + "/" + value.downcase.gsub(' ','_')
+            puts key
             new_translations[key] = value
           end
         end
@@ -39,10 +47,22 @@ namespace :burek do
       if File.exists?(translation_path + "test.#{locale}.yml")
         translations_hash = YAML::load_file(translation_path + "test.#{locale}.yml") #Load
       end
+
+      translations_hash[locale.dup.force_encoding("UTF-8")] = {} unless translations_hash.has_key?(locale.dup.force_encoding("UTF-8"))
       new_translations.each do |key,value|
-        translations_hash[key.dup.force_encoding("UTF-8")] = ( locale == locales.first ? value.dup.force_encoding("UTF-8") : translation_placeholder )
+        cur_hash = translations_hash[locale.dup.force_encoding("UTF-8")]
+        path_parts = key.split("/")
+        item_name = path_parts.pop
+        path_parts.each do |item|
+          cur_hash[item] = {} unless cur_hash.has_key?(item)
+
+          cur_hash = cur_hash[item]
+        end
+        cur_hash[item_name] = ( locale == locales.first ? value.dup.force_encoding("UTF-8") : translation_placeholder )
       end
-      File.write(translation_path + "test.#{locale}.yml", translations_hash.to_yaml) #Store
+
+      puts translations_hash.to_yaml
+      #File.write(translation_path + "test.#{locale}.yml", translations_hash.to_yaml) #Store
     end
 
   end
