9b98213f26f72923984a562471cc9b15529d6025
Filip Defar <dabrorius@gmail.com>
2013-09-26 17:28:15 +0200

    Refactored parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -3,16 +3,15 @@ module Burek
   module Parser
 
     def self.find_burek_calls(string)
-      string.scan(/[^a-zA-Z0-9_]burek[ \t]*\([ \t]*('|")(?<key>[^\)]*)('|")[^\)]*\)/).flatten 
+      string.scan(burek_call_regex).flatten 
     end
 
     def self.replace_burek_calls(contents, to_replace)
-      matches = Burek::Parser.find_burek_calls(contents)
+      matches = find_burek_calls(contents)
       changes_made = false
       matches.each do |value|
-        regex_str = "[^a-zA-Z0-9_]burek[ \\t]*\\([ \\t]*('|\")#{value}\('|\")[^\\)]*\\)"
         if to_replace.has_key?(value)
-          contents.gsub!(Regexp.new(regex_str)," t('#{to_replace[value]}')") 
+          contents.gsub!(burek_call_regex(value)," t('#{to_replace[value]}')") 
           changes_made = true
         end
       end
@@ -24,6 +23,11 @@ module Burek
       yaml.lines.to_a[1..-1].join
     end
 
+    private 
+
+    def self.burek_call_regex(caption="(?<key>[^\\)]*)")
+      Regexp.new("[^a-zA-Z0-9_]burek[ \\t]*\\([ \\t]*('|\")#{caption}\('|\")[^\\)]*\\)")
+    end
 
   end
 end
